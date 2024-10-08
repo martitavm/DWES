414fc8db0c46d288e55593ab01dff0d9b505e545
martitavm <mvilmag028@fp.iesromerovargas.com>
2024-10-08 09:24:02 +0200

    Initial commit

---
+++ comentarios/ejercicio_12.1.py
@@ -0,0 +1,7 @@
+''' Escriba operaciones de suma, resta, multiplicación y división que den como resultado el número 8. Asegúrese de
+incluir sus operaciones en llamadas a print() para ver los resultados. Debería crear cuatro líneas con este aspecto:'''
+
+print(4+4) # Suma
+print(10-2) # Resta
+print(4*2) # Multiplicación
+print(24/3) # División
---
+++ comentarios/ejercicio_12.py
@@ -0,0 +1,4 @@
+''' Use una variable para representar su número favorito. A continuación, usando esa variable, cree el mensaje que revele su número favorito e imprímalo. '''
+
+numero_favorito = 19 # Mi numero favorito
+print(f"Mi número favorito es: {numero_favorito}")
