66efdd8cba17535e2e1c62b04662010150fdc1ab
martitavm <mvilmag028@fp.iesromerovargas.com>
2024-10-10 13:29:34 +0200

    Initial commit

--- listas/ejercicio_18.py
+++ listas/ejercicio_18.py
@@ -1,3 +1,30 @@
-from listas.ejercicio_17 import lista
+lista = ["Shannis", "Paquita Salas","Rivas", "Belen Esteban", "Lady Gaga", "Ana", "Cupcakke", "Draculaura"]
 
-print(f"Lo siento,no puedes venir a mi fiesta {lista.pop}")
+print("Lamentablemente, solo puedo invitar a dos personas.")
+
+eliminado1 = lista.pop()
+print(f"Lo siento, {eliminado1}, pero no puedo invitarte a la cena.")
+
+eliminado2 = lista.pop()
+print(f"Lo siento, {eliminado2}, pero no puedo invitarte a la cena.")
+
+eliminado3 = lista.pop()
+print(f"Lo siento, {eliminado3}, pero no puedo invitarte a la cena.")
+
+eliminado4 = lista.pop()
+print(f"Lo siento, {eliminado4}, pero no puedo invitarte a la cena.")
+
+eliminado5 = lista.pop()
+print(f"Lo siento, {eliminado5}, pero no puedo invitarte a la cena.")
+
+eliminado6 = lista.pop()
+print(f"Lo siento, {eliminado6}, pero no puedo invitarte a la cena.")
+
+
+print("Sigues invitada a mi fiesta", lista[0])
+print("Sigues invitada a mi fiesta", lista[1])
+
+del lista[0]
+del lista[0]
+
+print("Lista de invitados vacía:", lista)
