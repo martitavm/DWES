f3faa4a07f06f86cd7a74bfb5b861e1ce29c5ccc
martitavm <mvilmag028@fp.iesromerovargas.com>
2025-01-16 19:42:22 +0100

    Actualizacion de codigo

--- libraryproject/books/models.py
+++ libraryproject/books/models.py
@@ -10,14 +10,14 @@ class Genre(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    birth_date = models.DateField()
+    birth_date = models.DateField(blank=True, null=False)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    Author = models.ManyToManyField(Author)
+    author = models.ManyToManyField(Author, related_name='books')
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     publish_date = models.DateField()
     summary = models.TextField()
